7971f0f6474859f56c9c71520e7124a583e33aaa
floppyist <floppyism@gmail.com>
2025-09-02 18:41:24 +0200

    Fixed vue test code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { rel } from 'vue';
+import { ref } from 'vue';
 
-const test = rel("test");
+const test = ref("test");
 </script>
 
 <template>
     <input v-model="test">
+    <p>{{ test }}</p>
 </template>
 
 <style scoped></style>
