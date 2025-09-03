cf0eaac511cb75ae82df5bee24d961c7318c8c57
floppyist <floppyism@gmail.com>
2025-09-03 10:05:20 +0200

    Added very simple table component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,6 +3,7 @@ import { useFilesStore } from '@/stores/FilesStore.js';
 import { useI18nStore } from '@/stores/I18nStore.js';
 
 import Navbar from '@/components/Navbar.vue';
+import IFMTable from '@/components/IFMTable.vue';
 
 const filesStore = useFilesStore();
 const i18nStore = useI18nStore();
@@ -19,6 +20,10 @@ if (!i18nStore.i18nData) {
 <template>
     <main class="flex flex-col h-screen bg-slate-300">
         <Navbar></Navbar>
+
+        <div class="flex-auto overflow-y-auto overflow-x-hidden">
+            <IFMTable></IFMTable>
+        </div>
     </main>
 </template>
 
---
+++ frontend/src/components/IFMTable.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { useFilesStore } from '@/stores/FilesStore.js';
+
+const filesStore = useFilesStore();
+
+if (!filesStore.clientData) {
+    filesStore.getFiles();
+}
+</script>
+
+<template>
+    <table class="w-full">
+        <thead class="bg-slate-600">
+            <tr>
+                <th>Filename</th>
+                <th></th>
+                <th>Size</th>
+                <th>Permissions</th>
+                <th>Owner</th>
+                <th>Group</th>
+            </tr>
+        </thead>
+        <tbody class="text-center">
+            <tr v-for="file in filesStore.filesData" :key="file.name">
+                <td>{{ file.name }}</td>
+                <td>[DOWNLOAD_BUTTON]</td>
+                <td>{{ file.size }}</td>
+                <td>{{ file.fileperms }}</td>
+                <td>{{ file.owner }}</td>
+                <td>{{ file.group }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
